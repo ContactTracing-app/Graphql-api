b90fd78c3046c13c5eb83b8dc203f81787c7224b
Michele Memoli <michele@100shapes.com>
2020-03-18 10:43:39 +0000

    Migrated to eslint

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,56 @@
-import { augmentTypeDefs, augmentSchema } from "neo4j-graphql-js";
-import { ApolloServer, gql, makeExecutableSchema } from "apollo-server";
-import { v1 as neo4j } from "neo4j-driver";
+import { augmentTypeDefs, augmentSchema } from 'neo4j-graphql-js';
+import { ApolloServer, makeExecutableSchema } from 'apollo-server';
+import { v1 as neo4j } from 'neo4j-driver';
 import {
   typeDefs as defaultTypedefs,
-  resolvers as defaultResolvers
-} from "./schema";
+  resolvers as defaultResolvers,
+} from './schema';
 
-require("dotenv").config();
+require('dotenv').config();
 
 const typeDefs = process.env.TYPEDEFS || defaultTypedefs;
 const resolvers = defaultResolvers;
 
 const schema = makeExecutableSchema({
   typeDefs: augmentTypeDefs(typeDefs),
-  // config: {
-  //   query: {
-  //     exclude: ["LogContactPayload"]
-  //   }
-  // },
+
   resolverValidationOptions: {
-    requireResolversForResolveType: false
+    requireResolversForResolveType: false,
   },
-  resolvers
+  resolvers,
 });
 
 // Add auto-generated mutations
-const augmentedSchema = augmentSchema(schema);
+const augmentedSchema = augmentSchema(schema, {
+  query: {
+    exclude: ['LogContactPayload'],
+  },
+});
 
 const driver = neo4j.driver(
-  process.env.NEO4J_URI || "bolt://localhost:7687",
+  process.env.NEO4J_URI || 'bolt://localhost:7687',
   neo4j.auth.basic(
-    process.env.NEO4J_USER || "neo4j",
-    process.env.NEO4J_PASSWORD || "letmein"
-  )
+    process.env.NEO4J_USER || 'neo4j',
+    process.env.NEO4J_PASSWORD || 'letmein',
+  ),
 );
 
 const server = new ApolloServer({
   schema: augmentedSchema,
   // inject the request object into the context to support middleware
   // inject the Neo4j driver instance to handle database call
-  context: ({ req }) => {
-    return {
-      driver,
-      req
-    };
-  }
+  context: ({ req }) => ({
+    driver,
+    req,
+  }),
 });
 
 server
   .listen(
     process.env.PORT || process.env.GRAPHQL_LISTEN_PORT || 3000,
-    "0.0.0.0"
+    '0.0.0.0',
   )
   .then(({ url }) => {
+    // eslint-disable-next-line no-console
     console.log(`GraphQL API ready at ${url}`);
   });
