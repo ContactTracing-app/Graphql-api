1b7a85e28aee47f1fdd3f3221e71a0fd19383d72
Tomaz Bratanic <bratanic.tomaz@gmail.com>
2020-04-05 17:20:10 +0200

    WiP

--- src/appConfigs/schema.ts
+++ src/appConfigs/schema.ts
@@ -48,6 +48,32 @@ export const typeDefs = gql`
       )
   }
 
+  type Place {
+    _id: ID!
+    uid: ID!
+    location: Point!
+    hasDay: [PlaceDay]!
+     @cypher(
+       statement: """
+       MATCH (this)-->(day:PlaceDay)
+       OPTIONAL MATCH (day)<-[:HAS_LOCATION]-(logEntry)
+       RETURN {date:day.date, visitors:collect(logEntry)}
+       """
+     )
+  }
+
+  type PlaceDay {
+    id: ID!
+    date: DateTime!
+    visitorLogs:[LogEntry]!
+     @cypher(
+      statement: """
+      MATCH (this)<-[:HAS_LOCATION]-(logEntry)
+      RETURN logEntry
+      """
+     )
+  }
+
   input PersonByIdInput {
     uid: ID!
   }
